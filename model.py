9176d1cc031efddc02829693d1f49dbbe156651c
Agustin Nieto <agusnieto.gg@gmail.com>
2022-12-07 22:29:29 -0500

    Code cleanup

--- model.py
+++ model.py
@@ -1,26 +1,6 @@
-## Importacion de librerías
 import pandas as pd
-import numpy as np 
-import plotly.express as px
-import datetime as dt
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
-from datetime import datetime
-from scipy.cluster.hierarchy import linkage, dendrogram
-import matplotlib.pyplot as plt
-import statsmodels.formula.api as smf
-import random
-from sklearn.covariance import LedoitWolf
-import sys
-from sklearn.covariance import empirical_covariance
-from scipy.spatial import distance
-from sklearn.mixture import GaussianMixture
-
-## Importacion datos
-# df = pd.read_csv('D:/nuevo/universidad/Master/Proyecto_Integrador/bdCompletarAgrupRefinada.csv',  parse_dates=[2], infer_datetime_format= True)
-# df["Unidades"] = pd.to_numeric(df["Unidades"])
-
-dfinal = pd.DataFrame()
 
 def predictInve(tienda,semana):
     ##Trae los datos con los que entrena el modelo, cambiar por las sentencias desde S3
@@ -47,13 +27,8 @@ def seman(Semana, df):
     tam = len(datSem)
     prd = pd.DataFrame(columns=['PRENDA','UNIDADES'],index=range(tam))
     for i in range(len(datSem)):
-        #print(datSem.iloc[i]['Cluster'])
         unidadesDat = df[df['Cluster'] == datSem.iloc[i]['Cluster']]
-        #print(datSem.iloc[i]['Prenda'])
         unidadesDatre = unidadesDat['UNIDADES'][unidadesDat['PRENDA'] == datSem.iloc[i]['PRENDA']]
         prd.iloc[i] = (datSem.iloc[i]['PRENDA'], unidadesDatre.median())
         
     return prd
-
-#dfinal = predictInve('MOLINOS 1',5)
-# print(dfinal)
