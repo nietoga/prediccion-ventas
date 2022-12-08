76749699af5396c200e3e3a39f10b4d038413cec
Agustin Nieto <agusnieto.gg@gmail.com>
2022-12-08 08:05:49 -0500

    Retrieve data source from env

--- app/model.py
+++ app/model.py
@@ -2,10 +2,12 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 
+import os
+
 
 def predictInve(tienda, semana):
     ##Trae los datos con los que entrena el modelo, cambiar por las sentencias desde S3
-    df = pd.read_csv('s3://predecir-ventas-data/bdCompletarAgrupRefinada.csv', parse_dates=[2],
+    df = pd.read_csv(os.getenv("DATABASE_SOURCE"), parse_dates=[2],
                      infer_datetime_format=True)
     df["UNIDADES"] = pd.to_numeric(df["UNIDADES"])
     dretu = pd.DataFrame()
