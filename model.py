fe49ebd13ab9809466e9a1ff49fca4b66df34091
Agustin Nieto <agusnieto.gg@gmail.com>
2022-12-07 23:23:19 -0500

    Format code

--- model.py
+++ model.py
@@ -2,36 +2,39 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 
-def predictInve(tienda,semana):
+
+def predictInve(tienda, semana):
     ##Trae los datos con los que entrena el modelo, cambiar por las sentencias desde S3
-    df = pd.read_csv('s3://predecir-ventas-data/bdCompletarAgrupRefinada.csv',  parse_dates=[2], infer_datetime_format= True)
+    df = pd.read_csv('s3://predecir-ventas-data/bdCompletarAgrupRefinada.csv', parse_dates=[2],
+                     infer_datetime_format=True)
     df["UNIDADES"] = pd.to_numeric(df["UNIDADES"])
     dretu = pd.DataFrame()
     datSem = df[df['TIENDA'] == tienda]
-    df_en = datSem.drop(columns = 'TIENDA')
+    df_en = datSem.drop(columns='TIENDA')
 
-    #Codificación de la columna Prendas
+    # Codificación de la columna Prendas
     encoder = preprocessing.LabelEncoder()
-    enc1 = encoder.fit(df_en['PRENDA'])  #no se guardan las clases
-    df_en['PRENDA']  = enc1.transform(df_en['PRENDA'])
+    enc1 = encoder.fit(df_en['PRENDA'])  # no se guardan las clases
+    df_en['PRENDA'] = enc1.transform(df_en['PRENDA'])
 
     # Kmeans
-    kmeans = KMeans(n_clusters = 25, random_state = 250).fit(df_en) #original df_en
+    kmeans = KMeans(n_clusters=25, random_state=250).fit(df_en)  # original df_en
     df_en['Cluster'] = kmeans.labels_
 
     # Volver a ponerle a las prendas su valor
     df_en['PRENDA'] = enc1.inverse_transform(df_en['PRENDA'])
 
-    dretu = seman(semana,df_en)
+    dretu = seman(semana, df_en)
     return dretu
+
 
 def seman(Semana, df):
     datSem = df[df['SEMANA'] == Semana]
     tam = len(datSem)
-    prd = pd.DataFrame(columns=['PRENDA','UNIDADES'],index=range(tam))
+    prd = pd.DataFrame(columns=['PRENDA', 'UNIDADES'], index=range(tam))
     for i in range(len(datSem)):
         unidadesDat = df[df['Cluster'] == datSem.iloc[i]['Cluster']]
         unidadesDatre = unidadesDat['UNIDADES'][unidadesDat['PRENDA'] == datSem.iloc[i]['PRENDA']]
         prd.iloc[i] = (datSem.iloc[i]['PRENDA'], unidadesDatre.median())
-        
+
     return prd
