27aeee354da73aa4c7f83d6c83775a885c8b2a76
Agustin Nieto <agusnieto.gg@gmail.com>
2022-12-08 08:05:27 -0500

    Remove unused code

--- app/main.py
+++ app/main.py
@@ -19,7 +19,3 @@ app.add_middleware(
 async def root(tienda: str, semana: int):
     result = predecir(tienda, semana)
     return result.to_dict(orient='records')
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, log_level="info")
