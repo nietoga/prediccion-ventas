95b48c65593c7a5690ee6ddd7b342ad1672b690f
Agustin Nieto <agusnieto.gg@gmail.com>
2022-12-08 06:00:56 -0500

    Add execution config

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -18,3 +19,7 @@ app.add_middleware(
 async def root(tienda: str, semana: int):
     result = predecir(tienda, semana)
     return result.to_dict(orient='records')
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, log_level="info")
